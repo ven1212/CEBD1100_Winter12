dd0ab9095c5cc17e6c781566ef3264c55f777a1c
venuya <venuya12@gmail.com>
2023-02-23 21:42:07 -0500

    Class 5

---
+++ Class 5 /Functions.py
@@ -0,0 +1,12 @@
+price = 100.00
+
+
+
+tax = price
+
+
+
+
+
+#tax = price + (price * 0.09775) + (price * 0.05)
+#tax = price +=
---
+++ Class 5 /list_tuples.py
@@ -0,0 +1,16 @@
+# Create a list, adding
+
+a = [1,2,3]
+b = ["A","B"]
+c = [ [80,72], [66,77] ]
+
+b.append("C")
+c.append(a)
+
+t = (7,8,9)
+#t[1] = 5
+t2 = list(t) t2: [7,8,9]
+
+print(a[0]
+
+print(t[2])
